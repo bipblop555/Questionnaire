fd02308ffbde8986bb9201fe3282100eb9f8e016
bipblop555 <tom.bonnet@hotmail.com>
2025-09-17 14:56:45 +0200

    ajout page2

---
+++ Questionnaire/Views/Page2.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Questionnaire.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+    }
+}
